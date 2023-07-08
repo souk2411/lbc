1346621687dac27c26928d979fce3de63fb379e9
ndiaye.serigne-modou-bousso@ugb.edu.sn <ndiaye.serigne-modou-bousso@ugb.edu.sn>
2023-07-08 23:39:23 +0000

    smbn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Category from "../components/Layouts/Categories";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Category />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
